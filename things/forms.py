bc2b784b142e3cde67a90e44c380f182833dd016
Brian <probrian89@gmail.com>
2023-10-08 05:58:52 +0800

    keats exercise 3

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,18 @@
 """Forms of the project."""
-
+from django import forms
+from .models import Thing
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    description = forms.CharField(widget=forms.Textarea)  # Display description as a Textarea
+    quantity = forms.IntegerField(widget=forms.NumberInput)
+
+    def clean_quantity(self):
+        quantity = self.cleaned_data['quantity']
+        if quantity < 0:
+            raise forms.ValidationError("Quantity must be a positive number.")
+        return quantity
--- things/views.py
+++ things/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .forms import ThingForm  # Import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        # If the form is submitted, process the data
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            # Save the valid form data to create a new Thing
+            form.save()
+            # You can also perform additional actions here
+    else:
+        # If it's a GET request, just display the empty form
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
